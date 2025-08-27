174921dfdd1e10f311e724dbc7c2aa48e0cb8047
betomoedano <betomoedano@outlook.com>
2025-08-27 10:38:58 -0500

    add context menu

--- app/home/_layout.tsx
+++ app/home/_layout.tsx
@@ -1,6 +1,5 @@
-import { Host, Image } from "@expo/ui/swift-ui";
+import ContextMenuProfile from "@/components/ContextMenu.ios";
 import { Stack } from "expo-router";
-import { Pressable } from "react-native";
 
 export default function HomeLayout() {
   return (
@@ -10,15 +9,34 @@ export default function HomeLayout() {
         options={{
           headerLargeTitle: true,
           headerTransparent: true,
-          headerBlurEffect: "light",
+          // headerBlurEffect: "light",
+          headerLargeStyle: {
+            backgroundColor: "transparent",
+          },
           title: "Home",
-          headerRight: ({ tintColor }) => (
-            <Pressable onPress={() => alert("test")}>
-              <Host style={{ width: 40, height: 40 }}>
-                <Image systemName="person.fill" color={tintColor} />
-              </Host>
-            </Pressable>
+          headerLeft: ({ tintColor }) => (
+            <ContextMenuProfile />
+
+            // <Pressable onPress={() => alert("test")}>
+            //   <Host
+            //     style={{ width: 35, height: 35 }}
+            //     modifiers={[
+            //       glassEffect({
+            //         glass: { variant: "regular", interactive: false },
+            //       }),
+            //     ]}
+            //   >
+            //     <Image systemName="person.fill" color={tintColor} />
+            //   </Host>
+            // </Pressable>
           ),
+          // headerRight: ({ tintColor }) => (
+          //   <Pressable onPress={() => alert("test")}>
+          //     <Host style={{ width: 40, height: 40 }}>
+          //       <Image systemName="person.fill" color={tintColor} />
+          //     </Host>
+          //   </Pressable>
+          // ),
         }}
       />
     </Stack>
