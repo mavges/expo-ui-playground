9df0ce7c4f29c2407ee82eac91a1ad5151e31d8a
betomoedano <betomoedano@outlook.com>
2025-09-12 12:05:42 -0500

    remove unnecessary comments

--- components/screens/liquid-glass-example.tsx
+++ components/screens/liquid-glass-example.tsx
@@ -1,7 +1,6 @@
 import { Form, Host } from "@expo/ui/swift-ui";
 import React from "react";
 
-// Component imports
 import { AppProvider } from "../liquid-glass/AppContext";
 import { ButtonsSection } from "../liquid-glass/ButtonsSection";
 import { ContextMenuSection } from "../liquid-glass/ContextMenuSection";
@@ -11,7 +10,6 @@ import { ProfileSection } from "../liquid-glass/ProfileSection";
 import { SettingsSection } from "../liquid-glass/SettingsSection";
 import { TaskManagementSection } from "../liquid-glass/TaskManagementSection";
 
-// Main component
 function AppContent() {
   return (
     <Host style={{ flex: 1 }}>
